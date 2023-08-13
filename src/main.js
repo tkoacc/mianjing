3ba10729d6e6d8af512c1f1c96a477992033ec73
TOP-Z\riley <riley211375@gmail.com>
2023-08-13 16:23:11 +0800

    components

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 
 // 导入amfe-flexible，监听视口变化，设置rem基准值1/10宽度
 import 'amfe-flexible'
+// 导入vant
 import '@/utils/vant.js'
+// 导入全局组件ArticleItem
+import ArticleItem from '@/components/ArticleItem.vue'
+Vue.component('ArticleItem', ArticleItem)
 
 Vue.config.productionTip = false
 
