b3a0f6b3f888a802af7a86551013ac06735bdbfc
TOP-Z\riley <riley211375@gmail.com>
2023-10-20 01:30:57 +0800

    change

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
   css: {
     loaderOptions: {
       less: {
